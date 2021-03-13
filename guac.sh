9499076b70a2a9c1eb0a3f0c51aaaba70527adfd
rkochhan <rkochhan@student.42sp.org.br>
2021-03-12 22:34:44 -0300

    reorganized script

--- guac.sh
+++ guac.sh
@@ -8,15 +8,15 @@ MY_REPO="https://raw.githubusercontent.com/rkrocha/guac_script/master/"
 # Esses são os arquivos que serão baixados de MY_REPO. Cada um desses nomes é concatenado ao fim de MY_REPO, formando um endereço utilizável.
 MY_FILES=".zshrc .vimrc ssh_init.sh .scripts/c .scripts/cft .scripts/cm .scripts/cmft"
 
-cd ~
-
-ls -1a | grep -q "\.scripts" || mkdir .scripts
-
 curl_file ()
 {
 	ls -1a | grep -q "$1" || curl -s "$MY_REPO$1" -o ~/"$1"
 	grep -q "^404: Not Found" "$1" && rm "$1" 1>/dev/null && echo "Curling file "$1" returned error 404: Not Found in repository"
 }
+
+cd ~
+
+ls -1a | grep -q "\.scripts" || mkdir .scripts
 
 for i in $MY_FILES
 do
